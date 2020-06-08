d74cbe4cfa494d932e3655075ea93240b3be9bf7
Monique.Bass01 <monique.bass01@gmail.com>
2020-06-08 10:38:34 -0500

    added single view

--- src/components/pages/SingleStuff/SingleStuff.js
+++ src/components/pages/SingleStuff/SingleStuff.js
@@ -1,12 +1,26 @@
 import React from 'react';
 
 import './SingleStuff.scss';
+import itemsData from '../../../helpers/data/itemsData';
 
 class SingleStuff extends React.Component {
+  state = {
+    item: {},
+  }
+
+  componentDidMount() {
+    const { itemId } = this.props.match.params;
+    itemsData.getSingleItem(itemId)
+      .then((response) => this.setState({ scat: response.data }))
+      .catch((err) => console.error('unable to get single item: ', err));
+  }
+
   render() {
+    const { item } = this.state;
     return (
       <div className="SingleStuff">
         <h1>Single Stuff</h1>
+    <p>Details: {item.itemDescription}</p>
       </div>
     );
   }
